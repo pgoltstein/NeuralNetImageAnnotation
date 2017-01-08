ccbe8522dbd1f1cabd97816a93ae01da653a11da
Pieter Goltstein <pgoltstein@mac.com>
2017-01-08 14:48:49 +0100

    conv net tools init function

--- iaConvNetTools.py
+++ iaConvNetTools.py
@@ -21,23 +21,34 @@ import tensorflow as tf
 ### Supporting functions
 ########################################################################
 
-class Conv2Net(object):
-    """Holds a 2 layer convolutional neural network for annotating
-    multi channel images"""
+class ConvNetCnv2Fc1(object):
+    """Holds a convolutional neural network for annotating
+    multi channel images.
+    2 convolutional layers, 1 fully connected layer, 1 output layer"""
 
-    def __init__(self, image_size, n_channels, output_size):
-        """Initializes all variables and sets up the network"""
-        self.y_res = image_size[0]
-        self.x_res = image_size[1]
-        self.n_input_channels = n_channels
+    def __init__(self, input_image_size, n_input_channels, output_size,
+                conv1_size=5, conv1_n_chan=32, conv1_n_pool=2,
+                conv2_size=5, conv2_n_chan=64, conv2_n_pool=2,
+                fc1_n_chan=1024 ):
+        """Initializes all variables and sets up the network
+        input_image_size: Tuple containing (y,x) size of input image
+        output_image_size: Tuple containing dimensions of network output"""
+        self.y_res = input_image_size[0]
+        self.x_res = input_image_size[1]
+        self.n_input_channels = n_input_channels
         self.out_y_res = output_size[0]
         self.out_x_res = output_size[1]
-        self.conv1_size = 5
-        self.conv1_n_chan = 32
-        self.conv1_n_pool = 2
-        self.conv2_size = 5
-        self.conv2_n_chan = 64
-        self.conv2_n_pool = 2
+        self.conv1_size = conv1_size
+        self.conv1_n_chan = conv1_n_chan
+        self.conv1_n_pool = conv1_n_pool
+        self.conv2_size = conv2_size
+        self.conv2_n_chan = conv2_n_chan
+        self.conv2_n_pool = conv2_n_pool
+        self.fc1_y_size = int( np.ceil( np.ceil(
+            self.y_res/self.conv1_n_pool ) / self.conv2_n_pool ) )
+        self.fc1_x_size = int( np.ceil( np.ceil(
+            self.x_res/self.conv1_n_pool ) / self.conv2_n_pool ) )
+        self.fc1_n_chan = fc1_n_chan
 
         #########################################################
         # Input and target variable placeholders
@@ -56,98 +67,98 @@ class Conv2Net(object):
         #########################################################
         # Set up convolutional layer 1
         # W = [im-height x im-width x n-input-channels x n-output-channels])
-        conv1_shape = [self.conv1_size, self.conv1_size,
+        self.conv1_shape = [self.conv1_size, self.conv1_size,
                        self.n_input_channels, self.conv1_n_chan]
-        W_conv1 = tf.Variable( tf.truncated_normal(
-                               shape=conv1_shape, stddev=0.1))
-        b_conv1 = tf.Variable( tf.constant( 0.1, shape=self.conv1_n_chan ) )
+        self.W_conv1 = tf.Variable( tf.truncated_normal(
+                               shape=self.conv1_shape, stddev=0.1))
+        self.b_conv1 = tf.Variable( tf.constant(0.1, shape=self.conv1_n_chan ))
 
         # Convolve x_image with the weight tensor
-        conv1_lin = tf.nn.conv2d( x_image, W_conv1,
+        self.conv1_lin = tf.nn.conv2d( x_image, self.W_conv1,
                                   strides=[1, 1, 1, 1], padding='SAME' )
 
         # Add bias and apply transfer function
-        conv1_relu = tf.nn.relu( conv1_lin + b_conv1 )
+        self.conv1_relu = tf.nn.relu( self.conv1_lin + self.b_conv1 )
 
         # Max pooling
-        kernel = [1, self.conv1_n_pool, self.conv1_n_pool, 1]
-        conv1_pool = tf.nn.max_pool( conv1_relu,
-                        ksize=kernel, strides=kernel, padding='SAME' )
+        self.conv1_kernel = [1, self.conv1_n_pool, self.conv1_n_pool, 1]
+        self.conv1_pool = tf.nn.max_pool( self.conv1_relu,
+            ksize=self.conv1_kernel, strides=self.conv1_kernel, padding='SAME')
 
         #########################################################
         # Convolutional layer 2
-        conv2_shape = [self.conv2_size, self.conv2_size,
+        self.conv2_shape = [self.conv2_size, self.conv2_size,
                        self.conv1_n_chan, self.conv2_n_chan]
-        W_conv2 = tf.Variable( tf.truncated_normal(
-                               shape=conv2_shape, stddev=0.1 ) )
-        b_conv2 = tf.Variable( tf.constant( 0.1, shape=self.conv2_n_chan ) )
+        self.W_conv2 = tf.Variable( tf.truncated_normal(
+                               shape=self.conv2_shape, stddev=0.1 ) )
+        self.b_conv2 = tf.Variable( tf.constant(0.1, shape=self.conv2_n_chan ))
 
         # Convolve x_image with the weight tensor
-        conv2_lin = tf.nn.conv2d( conv1_pool, W_conv2,
+        self.conv2_lin = tf.nn.conv2d( self.conv1_pool, self.W_conv2,
                                   strides=[1, 1, 1, 1], padding='SAME' )
 
         # Add bias and apply transfer function
-        conv2_relu = tf.nn.relu( conv2_lin + b_conv2 )
+        self.conv2_relu = tf.nn.relu( self.conv2_lin + self.b_conv2 )
 
         # Max pooling
-        kernel = [1, self.conv2_n_pool, self.conv2_n_pool, 1]
-        conv2_pool = tf.nn.max_pool( conv2_relu,
-                        ksize=kernel, strides=kernel, padding='SAME' )
+        self.conv2_kernel = [1, self.conv2_n_pool, self.conv2_n_pool, 1]
+        self.conv2_pool = tf.nn.max_pool( self.conv2_relu,
+            ksize=self.conv2_kernel, strides=self.conv2_kernel, padding='SAME')
+
 
         #########################################################
         # Densely Connected Layer
-        W_fc1 = weight_variable([9 * 9 * 64, 1024])
-        b_fc1 = bias_variable([1024])
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 9*9*64])
-        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+        # Weights and bias
+        self.fc1_shape = [self.fc1_y_size * self.fc1_x_size * self.conv2_n_chan,
+                          self.fc1_n_chan]
+        self.W_fc1 = tf.Variable( tf.truncated_normal(
+                               shape=self.fc1_shape, stddev=0.1 ) )
+        self.b_fc1 = tf.Variable( tf.constant(0.1, shape=self.fc1_n_chan ))
 
-        # Dropout during training will prevent overfitting
-        keep_prob = tf.placeholder(tf.float32)
-        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+        # Flatten output from conv2
+        self.conv2_pool_flat = tf.reshape(
+            self.conv2_pool, [-1, self.fc1_shape[0]] )
 
+        # Calculate network step
+        self.fc1_relu = tf.nn.relu( tf.matmul( self.conv2_pool_flat,
+            self.W_fc1) + self.b_fc1 )
+
+        # Set up dropout option for fc1
+        self.fc1_keep_prob = tf.placeholder(tf.float32)
+        self.fc1_relu_drop = tf.nn.dropout(self.fc1_relu, self.fc1_keep_prob)
+
+        #########################################################
         # Readout layer
-        W_fc2 = weight_variable([1024, 2])
-        b_fc2 = bias_variable([2])
-        y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+        # Weights and bias
+        self.fc_out_shape = [self.fc1_shape[0], self.out_y_res*self.out_x_res]
+        self.W_fc_out = tf.Variable( tf.truncated_normal(
+                                shape=self.fc_out_shape, stddev=0.1 ) )
+        self.b_fc_out = tf.Variable( tf.constant(0.1,
+                                shape=self.fc_out_shape[1] ))
 
-        # Define loss and optimizer
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-        train_step = tf.train.AdamOptimizer(5e-4).minimize(cross_entropy)
+        # Calculate network step
+        self.fc_out_lin = tf.matmul( self.fc1_relu_drop,
+                                     self.W_fc_out ) + self.b_fc_out
 
+        #########################################################
+        # Define cost function and optimizer algorithm
+        self.cross_entropy = tf.reduce_mean(
+                    tf.nn.softmax_cross_entropy_with_logits(
+                                            self.fc_out_lin, self.y_trgt ) )
+        self.train_step = tf.train.AdamOptimizer(5e-4).minimize(
+                                                        self.cross_entropy )
+
+        #########################################################
         # Define how to test trained model
-        network_prediction  = tf.cast( tf.argmax(y_conv,1), tf.float32 )
-        correct_prediction = tf.equal( tf.argmax(y_conv,1), tf.argmax(y_,1) )
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        self.network_prediction  = tf.cast( tf.argmax(
+                                        self.fc_out_lin, 1 ), tf.float32 )
+        self.is_correct_prediction = tf.equal( tf.argmax( self.fc_out_lin, 1 ),
+                                               tf.argmax( self.y_trgt, 1 ) )
+        self.accuracy = tf.reduce_mean( tf.cast(
+                                    self.is_correct_prediction, tf.float32 ) )
 
-        # Create saver op
-        saver = tf.train.Saver()
-
-        # Initialize and start session
+    def start(self):
+        # Initialize variables and start session
         init = tf.initialize_all_variables()
-        sess = tf.Session()
-        sess.run(init)
-
-        # Set up tensorboard
-        accuracy_summary = tf.scalar_summary("accuracy", accuracy)
-        merged_summary = tf.merge_all_summaries()
-        writer = tf.train.SummaryWriter("/tmp/roiconv_logs/run6", sess.graph)
-
-
-    # Weight initialization function
-    def weight_variable(shape):
-      initial = tf.truncated_normal(shape, stddev=0.1)
-      return tf.Variable(initial)
-
-    # Bias initialization function
-    def bias_variable(shape):
-      initial = tf.constant(0.1, shape=shape)
-      return tf.Variable(initial)
-
-    # Convolution
-    def conv2d(x, W):
-      return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-    # Max pooling
-    def max_pool_2x2(x):
-      return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                            strides=[1, 2, 2, 1], padding='SAME')
+        self.sess = tf.Session()
+        self.sess.run(init)
--- nnLearnCentroidDetection.py
+++ nnLearnCentroidDetection.py
@@ -20,100 +20,40 @@ import iaConvNetTools as cn
 
 
 ########################################################################
-### Load data
-########################################################################
+# Load data
 
 data_path = '/Users/pgoltstein/Dropbox/TEMP/DataSet'
+zoom_size = (36,36)
 ais = ia.AnnotatedImageSet()
 ais.load_data_dir(data_path)
 
 
 ########################################################################
-### Network architecture
-########################################################################
+# Set up network
+nn = cn.ConvNetCnv2Fc1( input_image_size=zoom_size, n_input_channels=3,
+                        output_size=(1,2) )
+print(nn)
 
-x = tf.placeholder(tf.float32, [None, 3*zoom_size[0]*zoom_size[1]])
-y_ = tf.placeholder(tf.float32, shape=[None, 2])
-
-# convert input image to tensor
-# x_image = [-1 x image-height x image-width x n-input-channels]
-x_image_temp = tf.reshape(x, [-1,3,36,36])
-x_image = tf.transpose(x_image_temp, [0,2,3,1])
-
-# First convolutional layer
-# W = [patch-height x patch-width x n-input-channels x n-output-channels])
-W_conv1 = weight_variable([5, 5, 3, 32])
-b_conv1 = bias_variable([32])
-
-# convolve x_image with the weight tensor, add the bias,
-# apply the ReLU function, and finally max pool
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
-# Second convolutional layer
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-# Densely Connected Layer
-W_fc1 = weight_variable([9 * 9 * 64, 1024])
-b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 9*9*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-# Dropout during training will prevent overfitting
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-# Readout layer
-W_fc2 = weight_variable([1024, 2])
-b_fc2 = bias_variable([2])
-y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-
-# Define loss and optimizer
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-train_step = tf.train.AdamOptimizer(5e-4).minimize(cross_entropy)
-
-# Define how to test trained model
-network_prediction  = tf.cast( tf.argmax(y_conv,1), tf.float32 )
-correct_prediction = tf.equal( tf.argmax(y_conv,1), tf.argmax(y_,1) )
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-# Create saver op
-saver = tf.train.Saver()
-
-# Initialize and start session
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
-
-# Set up tensorboard
-accuracy_summary = tf.scalar_summary("accuracy", accuracy)
-merged_summary = tf.merge_all_summaries()
-writer = tf.train.SummaryWriter("/tmp/roiconv_logs/run6", sess.graph)
-
-#saver.restore(sess,"/tmp/roi_conv_model2.ckpt")
-
-# Train for 100 steps
-start = time.time()
-for t in range(3000):
-    if t % 10 != 0: # Just train
-        samples,labels = get_roi_training_data( im0_norm, im1_norm, im2_norm,
-                                                masked_roi_im, zoom_size, 200 )
-        sess.run(train_step,
-            feed_dict={x: samples, y_: labels, keep_prob: 0.5})
-        print('.', end="", flush=True)
-    else: # Dipslay progress
-        samples,labels = get_roi_training_data( im0_norm, im1_norm, im2_norm,
-                                                masked_roi_im, zoom_size, 200 )
-        result = sess.run([merged_summary,accuracy],
-            feed_dict={x: samples, y_: labels, keep_prob: 1.0})
-        summary_str = result[0]
-        acc = result[1]
-        writer.add_summary(summary_str, t)
-        end = time.time()
-        print('Step {:4d}: Acc = {:6.4f} (t={})'.format(
-                t, acc, str(datetime.timedelta(seconds=np.round(end-start))) ))
-        save_path = saver.save(sess, "/tmp/roi_conv_model3.ckpt")
-        print('  -> Model saved in file: {}'.format(save_path))
+# ########################################################################
+# # Train for 100 steps
+# start = time.time()
+# for t in range(3000):
+#     if t % 10 != 0: # Just train
+#         samples,labels = get_roi_training_data( im0_norm, im1_norm, im2_norm,
+#                                                 masked_roi_im, zoom_size, 200 )
+#         sess.run(train_step,
+#             feed_dict={x: samples, y_: labels, keep_prob: 0.5})
+#         print('.', end="", flush=True)
+#     else: # Dipslay progress
+#         samples,labels = get_roi_training_data( im0_norm, im1_norm, im2_norm,
+#                                                 masked_roi_im, zoom_size, 200 )
+#         result = sess.run([merged_summary,accuracy],
+#             feed_dict={x: samples, y_: labels, keep_prob: 1.0})
+#         summary_str = result[0]
+#         acc = result[1]
+#         writer.add_summary(summary_str, t)
+#         end = time.time()
+#         print('Step {:4d}: Acc = {:6.4f} (t={})'.format(
+#                 t, acc, str(datetime.timedelta(seconds=np.round(end-start))) ))
+#         save_path = saver.save(sess, "/tmp/roi_conv_model3.ckpt")
+#         print('  -> Model saved in file: {}'.format(save_path))
