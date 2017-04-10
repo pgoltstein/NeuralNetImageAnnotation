afb5cda97aa797bcfe27978b305c78382f2c189c
Pieter Goltstein <pgoltstein@mac.com>
2017-04-10 12:02:38 +0200

    changed to deal with body annotations

--- nnLearn1StepBodyDetection.py
+++ nnLearn1StepBodyDetection.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 6 17:49:54 2017
+Created on April 10 2017
 
-Contains functions that detect centroids of annotations
+Contains functions that detect bodies of annotations in a single step
 
 @author: pgoltstein
 """
@@ -21,7 +21,7 @@ import iaConvNetTools as cn
 
 ########################################################################
 # Load data
-annotation_size = (27,27)
+annotation_size = (36,36)
 training_data_path = '/Users/pgoltstein/Dropbox/TEMP/DataSet1'
 network_path = '/Users/pgoltstein/Dropbox/TEMP'
 print("\nLoading data from directory into training_image_set:")
@@ -31,31 +31,33 @@ training_image_set.load_data_dir_tiff_mat(training_data_path)
 print(" >> " + training_image_set.__str__())
 
 # Dilate centroids
-print("Changing centroid dilation factor of image set to 2")
-training_image_set.centroid_dilation_factor = 2
+print("Changing body dilation factor of the image set to -3")
+training_image_set.body_dilation_factor = -3
 
 ########################################################################
 # Set up network
 nn = cn.ConvNetCnv2Fc1( \
         input_image_size=annotation_size,
         n_input_channels=training_image_set.n_channels, output_size=(1,2),
-        conv1_size=5, conv1_n_chan=16, conv1_n_pool=3,
-        conv2_size=5, conv2_n_chan=32, conv2_n_pool=3,
+        conv1_size=5, conv1_n_chan=16, conv1_n_pool=2,
+        conv2_size=5, conv2_n_chan=32, conv2_n_pool=2,
         fc1_n_chan=256, fc1_dropout=0.5, alpha=4e-4 )
 nn.start()
-nn.load_network_parameters('centroid_net',network_path)
+# nn.load_network_parameters('body1step_net',network_path)
 
 ########################################################################
 # Train network and save network parameters
 nn.train_epochs( training_image_set,
-    annotation_type='Centroids', m_samples=500, n_epochs=2500)
-nn.save_network_parameters('centroid_net',network_path)
+    annotation_type='Bodies', m_samples=500, n_epochs=2500,
+    exclude_border=(40,40,40,40))
+nn.save_network_parameters('body1step_net',network_path)
 
 ########################################################################
 # Display performance
 print("\nTraining set performance:")
 nn.report_F1( training_image_set,
-    annotation_type='Centroids', m_samples=2000, show_figure='On')
+    annotation_type='Bodies', m_samples=2000,
+    exclude_border=(40,40,40,40), show_figure='On')
 
 ########################################################################
 # Test morphed performance
@@ -64,8 +66,8 @@ rotation_list = np.array(range(360))
 scale_list_x = np.array(range(900,1100)) / 1000
 scale_list_y = np.array(range(900,1100)) / 1000
 noise_level_list = np.array(range(200)) / 10000
-nn.report_F1( training_image_set, annotation_type='Centroids',
-        m_samples=5000, exclude_border=(40,40,40,40), morph_annotations=True,
+nn.report_F1( training_image_set, annotation_type='Bodies',
+        m_samples=2000, exclude_border=(40,40,40,40), morph_annotations=True,
         rotation_list=rotation_list, scale_list_x=scale_list_x,
         scale_list_y=scale_list_y, noise_level_list=noise_level_list,
         show_figure='On')
