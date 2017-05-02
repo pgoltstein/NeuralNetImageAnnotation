improved normalization, scaling from 0-1; fixed capitalization bug in centroid and bodies dilation argument; fixed color display for 1 or 2 channels

ImageAnnotation.py
@@ -101,12 +101,14 @@ def zoom( image, y, x, zoom_size, normalize=False, pad_value=0 ):
             image_temp[0:image.shape[0],0:image.shape[1]] = image
             if normalize:
                 zoom_im = image_temp[ np.ix_(ix_y,ix_x) ]
+                zoom_im = zoom_im - zoom_im.min()
                 return zoom_im / zoom_im.max()
             else:
                 return image_temp[ np.ix_(ix_y,ix_x) ]
         else:
             if normalize:
                 zoom_im = image[ np.ix_(ix_y,ix_x) ]
+                zoom_im = zoom_im - zoom_im.min()
                 return zoom_im / zoom_im.max()
             else:
                 return image[ np.ix_(ix_y,ix_x) ]
@@ -332,7 +334,7 @@ def get_labeled_pixel_coordinates( bin_image, exclude_border=(0,0,0,0) ):
 
     # Get lists with all pixel coordinates
     y_res,x_res = bin_image.shape
-    (pix_x,pix_y) = np.meshgrid(np.arange(y_res),np.arange(x_res))
+    (pix_y,pix_x) = np.meshgrid(np.arange(y_res),np.arange(x_res))
 
     # Get lists with coordinates of all labeled pixels
     lab_pix_x = pix_x.ravel()[bin_image.ravel() == 1]
@@ -715,6 +717,7 @@ class AnnotatedImage(object):
             for ch in use_channels:
                 im_x = np.float64(np.array(mat_data['Images'][0,ch]))
                 if normalize:
+                    im_x = im_x - im_x.min()
                     im_x = im_x / im_x.max()
                 if self.downsamplingfactor is not None:
                     self._channel.append( ndimage.interpolation.zoom( \
@@ -734,6 +737,7 @@ class AnnotatedImage(object):
                 for ch in use_channels:
                     im_x = im[:,:,ch]
                     if normalize:
+                        im_x = im_x - im_x.min()
                         im_x = im_x / im_x.max()
                     if self.downsamplingfactor is not None:
                         self._channel.append( ndimage.interpolation.zoom( \
@@ -742,6 +746,7 @@ class AnnotatedImage(object):
                         self._channel.append(im_x)
             else:
                 if normalize:
+                    im = im - im.min()
                     im = im / im.max()
                 if self.downsamplingfactor is not None:
                     self._channel.append( ndimage.interpolation.zoom( \

test_annotated_image_set.py
@@ -22,9 +22,10 @@ matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 
 # Overall settings
-data_path = '/data/DataSet_small'
+data_path = '/data/EGR/Dataset1'
+# data_path = '/data/DataSet_small'
 im_size = (41,41)
-annot_size = (27,27)
+annot_size = (41,41)
 rotation_list = np.arange(0,360,1)
 scale_list_x = np.arange(0.9,1.1,0.01)
 scale_list_y = np.arange(0.9,1.1,0.01)
@@ -112,7 +113,7 @@ samples,labels,annotations = ais1.data_sample( \
         m_samples=30,
         # sample_ratio=None,
         sample_ratio=(0.4,0.4,0.2),
-        annotation_border_ratio=0.5,
+        annotation_border_ratio=0.5, normalize_samples=True,
         segment_all=True, morph_annotations=False,
         rotation_list=rotation_list, scale_list_x=scale_list_x,
         scale_list_y=scale_list_y, noise_level_list=noise_level_list )