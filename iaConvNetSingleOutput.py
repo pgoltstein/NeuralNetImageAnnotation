7911b9884fa1c8b8c4ec4d55325d1ccc79f584ef
Pieter Goltstein <pgoltstein@mac.com>
2017-08-10 14:06:49 +0200

    changed warning level of tensorflow

--- iaConvNetSingleOutput.py
+++ iaConvNetSingleOutput.py
@@ -21,8 +21,8 @@ import time, datetime
 import os
 import ImageAnnotation as ia
 
-tf.logging.set_verbosity(tf.logging.WARN)
-
+# tf.logging.set_verbosity(tf.logging.WARN)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 ########################################################################
 ### Single output neural network class
