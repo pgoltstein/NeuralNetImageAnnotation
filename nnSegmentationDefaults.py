51ab1b6351f6c08f166de9966367b69dae6a4467
Pieter Goltstein <pgoltstein@Pieters-MacBook-Pro.local>
2018-01-03 14:05:32 +0100

    updated defualts

--- nnSegmentationDefaults.py
+++ nnSegmentationDefaults.py
@@ -1,20 +1,20 @@
 # Default Settings
-network_path = '/data/nn'
-training_data_path = '/data/roi/Dataset_small'
-# network_path = 'D:/neuralnets'
-# training_data_path = 'D:/data/roi/DataSet_small1'
+# network_path = '/data/nn'
+# training_data_path = '/data/roi/Dataset_small'
+network_path = 'D:/neuralnets'
+training_data_path = 'D:/data/roi/DataSet_small1'
 
 # Annotation arguments
 selection_type = "Bodies"
 segment_all = False
-image_size = 31 # Size of the images around the annotations
-annotation_size = 21 # Size of the annotations to be made
+image_size = 41 # Size of the images around the annotations
+annotation_size = 31 # Size of the annotations to be made
 morph_annotations = False # Random morphing of annotations
 include_annotation_typenrs = 1 # Include only annotations of certain type_nr
 centroid_dilation_factor = 2 # Dilation/erosion (negative) of annotation centroid
 body_dilation_factor = 0 # Dilation/erosion (negative) of annotation body
-outline_thickness = 1 # Thickness of annotation outline
-sample_ratio = [0.5,0.5] # Fraction samples per class (None=all equal).
+outline_thickness = 2 # Thickness of annotation outline
+sample_ratio = [0.3,0.7] # Fraction samples per class (None=all equal).
 annotation_border_ratio = None # Fraction from border between pos and neg samples
 use_channels = [1,2] # List of channels that are to be used, 1 based e.g. [1,2]
 normalize_samples = False # True/False
@@ -22,10 +22,10 @@ downsample_image = None # downsampling of image by factor of #
 
 # Training arguments
 training_procedure = "batch" # "batch" or "epochs"
-n_epochs = 10
+n_epochs = 25
 m_samples = 500
 number_of_batches = 10
-batch_size = 2000
+batch_size = 5000
 report_every = 1
 fc_dropout = 0.5 # Keep-fraction in last fully connectd layer during training
 alpha = 0.005 # Learning rate (typically smaller than 0.001)
